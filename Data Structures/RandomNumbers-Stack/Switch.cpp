13860641624de3701482d03a9d7203ba3468a608
knyeow <ege.yasar37@gmail.com>
2022-10-09 18:04:26 +0300

    Create Switch.cpp

---
+++ Data Structures/RandomNumbers-Stack/Switch.cpp
@@ -0,0 +1,83 @@
+﻿#include <iostream>
+#include <ctime>
+
+
+using namespace std;
+
+
+bool isFound = false;
+
+
+
+
+ class Heap {
+	int* arr;
+	int last;
+	int capacity;
+public:
+	Heap(int size) {
+		arr = new int[size];
+		capacity = size;
+		last = -1;
+	};
+
+
+	int size() {
+		return capacity;
+	};
+	int top() {
+		if (last != -1)
+			return arr[last];
+		else return 0;
+
+	};
+
+
+	bool empty() {
+		return last == -1;
+	};
+
+
+	void pop() {
+		if (last != -1)   
+		last--;
+		capacity--;
+	};
+
+	void push(int x) {
+		arr[last + 1] = x;
+		last++;		
+	};
+	
+
+
+};
+
+ void checkHeap(Heap heap,int prediction){
+	 while (!heap.empty())
+	 {
+		 if (heap.top() == prediction) {
+			 cout <<"------------------" << heap.size() << ". number is " << prediction << endl;
+			 return;
+		 }
+		 else
+			 heap.pop();
+
+	 }
+
+}
+
+ int main()
+ {
+
+	 srand(time(NULL));
+
+	 Heap heap(100);
+	 for (int i = 0; i < 100; i++) {
+		 heap.push(rand() % 1001);
+
+	 }
+	 checkHeap(heap, 68);
+
+ }
+
